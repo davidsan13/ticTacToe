71c6ea25d9b7e0deca8563638ac8dfc63a5a9207
davidsan13 <sandavid08@yahoo.com>
2022-05-10 21:53:24 -0700

    added get and post method on gameboard

--- script.js
+++ script.js
@@ -1,42 +1,60 @@
+const xMark = "X"
+const oMark = 'O'
+
 const game = (function() {
     const gameBoard = document.querySelector(".gameboard");
     let board = ["X ", "O ", " ", " ", " ", " ", " ", " ", " ", ];
-    console.log(gameBoard);
 
-    
+    const setMark = (index, mark) => {
+        board[index] = mark;
+    };
+
+    const getMark = (index) => {
+        return board[index]
+    };
+
     function render() {
         var counter = 0;
         board.forEach(function(item) {
             const newdiv = document.createElement("div");
             newdiv.innerHTML = item;
-            newdiv.classList.add("grid");
-            newdiv.setAttribute('data-index', counter)
+            newdiv.classList.add("cell");
+            newdiv.setAttribute('data-cell', counter)
             gameBoard.appendChild(newdiv);
             counter++
             console.log(item);
         });
 
     }
-    function updateBoard() {
-
-    }
+    
     return {
-        board, 
+        setMark,
+        getMark,
         render,
-    };
+        
 })();
 
 game.render()
 
-const gridItems = document.querySelectorAll('.grid');
-gridItems.forEach((item) => { 
+const gridItems = document.querySelectorAll('.cell');
+gridItems.forEach(item => { 
     item.addEventListener('click', (e) => {
-        console.log(e.target.dataset.index)
+        console.log(e.target.classList.add(xMark))
+        let cell1 = e.target.dataset.cell;
+        game.board[cell1] = xMark;
+        game.removeCell();
+        game.render();
+        console.log(e.target.dataset.cell)
+        
     })
 });
 
 
-const player = (playerName, marker) => {
+let player = (playerName, marker) => {
     playerName
     marker
+}
+
+const displayController = () => {
+
 }
